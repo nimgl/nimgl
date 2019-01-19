52fca5e040da614c8618566eead8a6103975a28d
Leonardo Mariscal <leo@ldmd.mx>
2019-01-19 13:22:58 -0600

    Removed pragma unchecked from the carray since pragmas before generic list are deprecated

--- src/nimgl/imgui.nim
+++ src/nimgl/imgui.nim
@@ -407,7 +407,7 @@ type
   ImU64* = uint64
   ImWchar* = uint16
 
-  carray* {.unchecked.}[T] = UncheckedArray[T]
+  carray*[T] = UncheckedArray[T]
   ImDrawData* {.importc: "ImDrawData", header: "<cimgui.h>".} = object
     valid* {.importc: "Valid".} : bool
     cmdLists* {.importc: "CmdLists".} : carray[ptr ImDrawList]
