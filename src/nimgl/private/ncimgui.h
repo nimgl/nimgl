161aee55f0efddef42fc31131677e6ff137fc6b0
Leonardo Mariscal <leo@ldmd.mx>
2021-08-28 14:59:40 -0500

    Delete submodules for C repos

--- src/nimgl/private/ncimgui.h
+++
@@ -1,6 +0,0 @@
-#include "imgui/imgui.h"
-#include "imgui/imgui_internal.h"
-#include "cimgui.h"
-
-// Should open an issue in cimgui about this...
-// This isn't best practice in c++ so bear with me.
